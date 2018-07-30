3aa8f7168237a743faeece13970aa0534e2cac8f
Ilya <ilia.nikulin@gmail.com>
2018-07-30 18:02:24 +0300

    user tables

--- database/migrations/2018_07_30_145040_create_role_user_table.php
+++ database/migrations/2018_07_30_145040_create_role_user_table.php
@@ -13,7 +13,7 @@ class CreateRoleUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_role', function (Blueprint $table) {
+        Schema::create('role_user', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateRoleUserTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_role');
+        Schema::dropIfExists('role_user');
     }
 }
